83655fcc51b8553003cae39cf9ea54f36b7c4f36
cheng66663420 <569253615@qq.com>
2024-08-19 15:45:24 +0800

    xg

--- data_moudle.py
+++ data_moudle.py
@@ -11,6 +11,32 @@ def get_quote_data(
     period: str,
     data_dir: str = "D:/qmt_datadir",
 ) -> pd.DataFrame:
+    """
+    获取指定时间区间的股票数据。
+
+    Parameters
+    ----------
+    ticker_symbol : str
+        股票代码
+    start_time : str
+        开始时间
+    end_time : str
+        结束时间
+    period : str
+        周期
+    data_dir : _type_, optional
+        数据地址, by default "D:/qmt_datadir"
+
+    Returns
+    -------
+    pd.DataFrame
+        包含指定时间区间的股票数据的DataFrame
+
+    Raises
+    ------
+    FileNotFoundError
+        如果指定的文件不存在，则抛出此异常
+    """
     file_path = os.path.join(data_dir, period, ticker_symbol)
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"File not found: {file_path}")
@@ -35,6 +61,52 @@ def get_quote_data(
     ].compute()
 
     return result_df.sort_values(by="trade_time")
+
+
+def resample_stock_data(df: pd.DataFrame, resample_period: str = "30T") -> pd.DataFrame:
+    """
+    对股票数据进行重采样。
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        包含股票数据的DataFrame
+    resample_period : str, optional
+        重采样周期, by default "30T"
+
+    Returns
+    -------
+    pd.DataFrame
+        重采样后的股票数据的DataFrame
+    """
+    df = df.copy()
+    df["trade_time"] = pd.to_datetime(df["trade_time"])
+    df.set_index("trade_time", inplace=True)
+    result_list = []
+    for ticker_symbol, df_grouped in df.groupby("ticker_symbol"):
+        # 重采样并聚合数据
+        resampled_df = (
+            df_grouped.resample(resample_period)
+            .agg(
+                {
+                    "open": "first",
+                    "high": "max",
+                    "low": "min",
+                    "close": "last",
+                    "volume": "sum",
+                    "amount": "sum",
+                    "settelement_price": "last",
+                    "open_interest": "last",
+                    "preclose": "last",
+                    "suspend_flag": "last",
+                }
+            )
+            .reset_index()
+        )
+        # 将 ticker_symbol 列插入到第二列
+        resampled_df.insert(1, "ticker_symbol", ticker_symbol)
+        result_list.append(resampled_df)
+    return pd.concat(result_list)
 
 
 if __name__ == "__main__":
