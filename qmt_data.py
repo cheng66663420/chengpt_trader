7be975bfb838b014e65d8a6c71f9e3cd9e4d7c8f
cheng66663420 <569253615@qq.com>
2024-08-19 21:45:53 +0800

    xg

--- qmt_data.py
+++ qmt_data.py
@@ -227,28 +227,35 @@ class QmtData:
 
 
 if __name__ == "__main__":
+    import datetime
+
     qmt_data = QmtData()
-    ticker_df = qmt_data.get_tickers(["沪深基金"])
-    stock_list = ["159941.SZ"]
-    start_time = "20100101"
-    end_time = "20240815"
-    period = "1d"
-    qmt_data.download_data(
-        start_time=start_time, end_time=end_time, period=period, stock_list=stock_list
-    )
-    record = qmt_data.write_to_ftr_parallel(
-        stock_list=stock_list,
-        start_time=start_time,
-        end_time=end_time,
-        period=period,
-    )
-    record_df = pd.DataFrame(record)
-    os.makedirs(
-        "D:/qmt_datadir/logging/",
-        exist_ok=True,
-    )
-    record_df.to_excel(
-        f"D:/qmt_datadir/logging/{end_time}_record_{period}.xlsx",
-        index=False,
-        engine="openpyxl",
-    )
+    ticker_df = qmt_data.get_tickers()
+    stock_list = ticker_df["ticker"].tolist()
+    stock_list.sort()
+    today = datetime.datetime.today().strftime("%Y%m%d")
+    start_time = today
+    end_time = today
+    for period in ["1m", "5m", "1d"]:
+        qmt_data.download_data(
+            start_time=start_time,
+            end_time=end_time,
+            period=period,
+            stock_list=stock_list,
+        )
+        record = qmt_data.write_to_ftr_parallel(
+            stock_list=stock_list,
+            start_time=start_time,
+            end_time=end_time,
+            period=period,
+        )
+        record_df = pd.DataFrame(record)
+        os.makedirs(
+            "D:/qmt_datadir/logging/",
+            exist_ok=True,
+        )
+        record_df.to_excel(
+            f"D:/qmt_datadir/logging/{end_time}_record_{period}.xlsx",
+            index=False,
+            engine="openpyxl",
+        )
