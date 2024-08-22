9bde20b7cd4f1df2915660a998ad436f4f922f62
cheng66663420 <569253615@qq.com>
2024-08-22 22:31:53 +0800

    xg

--- new.py
+++ new.py
@@ -1,43 +1,41 @@
-import pandas as pd
-import dask
-from dask.distributed import Client
-from dask.diagnostics import ProgressBar
 import os
-from tqdm import tqdm
-import dask.dataframe as dd
+import pandas as pd
 
 
-@dask.delayed
-def read_file(filename):
-    return pd.read_parquet(filename)
+def main():
+    # 设置输入和输出路径
+    input_root_path = "d:/qmt_datadir/"
+    output_root_path = "G:/qmt_datadir/"
+    periods = ["1m", "5m", "1d"]
+    # 创建Dask客户端
+    # 这将使用所有可用的本地核心
+    for period in periods:
+        input_path = os.path.join(input_root_path, period)
+        output_path = os.path.join(output_root_path, period)
+        os.makedirs(output_path, exist_ok=True)
+        subdirs = [
+            d
+            for d in os.listdir(input_path)
+            if os.path.isdir(os.path.join(input_path, d))
+        ]
+        groups = [subdirs[i : i + 50] for i in range(0, len(subdirs), 50)]
+        for num, group in enumerate(groups):
+            dfs = []
+            print(f"合并第{num}组开始！")
+            for ticker in group:
+                path = os.path.join(input_path, ticker)
+                files = os.listdir(path)
+                for file in files:
+                    dfs.append(pd.read_parquet(os.path.join(path, file)))
 
+            merged_df = pd.concat(dfs)
+            merged_df.to_parquet(
+                os.path.join(output_path, f"merged_{num}.parquet"), engine="pyarrow"
+            )
+            print(f"合并第{num}组完成！")
 
-@dask.delayed
-def write_file(file_path, period, file):
-    temp_path = os.path.join(file_path, period, file)
-    print(temp_path)
-    parquet_list = [os.path.join(temp_path, file) for file in os.listdir(temp_path)]
-
-    df_list = [read_file(file) for file in parquet_list]
-
-    # 计算延迟对象
-    df_list = dask.compute(*df_list)
-    df = pd.concat(df_list)
-    df.sort_values(by="trade_time", inplace=True)
-    save_path = f"G:/qmt_datadir/{period}"
-    os.makedirs(save_path, exist_ok=True)
-    df.to_parquet(f"{save_path}/{file}.parquet", compression="snappy")
-    return None
+        print("合并完成！")
 
 
 if __name__ == "__main__":
-    client = Client()
-    root_path = "D:/qmt_datadir/"
-    period_list = ["1m", "5m", "1d"]
-    result = []
-    for period in tqdm(period_list):
-        file_path = os.path.join(root_path, period)
-        file_list = list(os.listdir(file_path))
-        for file in tqdm(file_list, leave=True):
-            result.append(write_file(root_path, period, file))
-    dask.compute(*result)
+    main()
